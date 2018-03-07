95db10ba07bc2830e193f40a2a07e034b4ce7b37
Andrea Carriero <andrea@cyberianetwork.com>
2018-03-08 00:43:18 +0100

    Test api responses

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,15 @@
-import unittest
+import unittest, json
 from app import app
 from toolbox.database import db
+from toolbox.configuration_loader import AppConfiguration
 
 from data_processing import populate_carriers_connectivity
 from models.carriers import CarrierConnectivity
 
 from data_processing import populate_carriers
 from models.carriers import Carrier
+
+conf = AppConfiguration()
 
 class TestCase(unittest.TestCase):
 
@@ -43,3 +46,28 @@ class TestCase(unittest.TestCase):
 
         self.assertEqual(cX, -5.088856115301341)
         self.assertEqual(cY, 48.45657455881529)
+
+    def test_legit_api_request(self):
+        r = self.app_instance.test_client().get('/', query_string={'q': 'Ouessant'})
+        self.assertEqual(r.status_code, 200)
+        rj = json.loads(r.data)
+        self.assertEqual(rj['meta']['location']['city'], 'Ouessant')
+        for carrier in conf.get('carriersToShow'):
+            self.assertIn(carrier, rj)
+
+    def test_legit_api_request_for_all_carriers(self):
+        r = self.app_instance.test_client().get('/', query_string={'q': 'Ouessant', 'all': True})
+        self.assertEqual(r.status_code, 200)
+        rj = json.loads(r.data)
+        self.assertEqual(rj['meta']['location']['city'], 'Ouessant')
+        with self.app_instance.test_request_context():
+            for carrier in Carrier.query.filter(Carrier.name != None).all():
+                self.assertIn(carrier.name, rj)
+
+    def test_bad_api_request(self):
+        r = self.app_instance.test_client().get('/', query_string={'query': 'Ouessant', 'all': True})
+        self.assertEqual(r.status_code, 400)
+
+    def test_wrong_place_api_request(self):
+        r = self.app_instance.test_client().get('/', query_string={'q': 'porto cesareo, lecce, italy', 'all': True})
+        self.assertEqual(r.status_code, 400)

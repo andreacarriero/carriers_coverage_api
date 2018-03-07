8222b1faf2d182a541c8fd8d53988ce4920bb810
Andrea Carriero <andrea@cyberianetwork.com>
2018-03-07 23:48:51 +0100

    Get connectivity for all carriers in specific city

--- models/carriers.py
+++ models/carriers.py
@@ -1,4 +1,7 @@
 from toolbox.database import db
+from toolbox.logger import get_logger
+
+log = get_logger(__name__)
 
 class Carrier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,10 +21,58 @@ class CarrierConnectivity(db.Model):
     conn_3G = db.Column(db.Boolean)
     conn_4G = db.Column(db.Boolean)
 
-    def __init__(self, mcc_mnc, coord_x, coord_y, conn_2G=False, conn_3G=False, conn_4G=False):
+    location_label = db.Column(db.String(50))
+    location_context = db.Column(db.String(50))
+    location_city = db.Column(db.String(50))
+
+    def __init__(
+        self,
+        mcc_mnc,
+        coord_x,
+        coord_y,
+        location_label,
+        location_context,
+        location_city,
+        conn_2G=False,
+        conn_3G=False,
+        conn_4G=False
+    ):
         self.mcc_mnc = mcc_mnc
         self.coord_x = coord_x
         self.coord_y = coord_y
         self.conn_2G = conn_2G
         self.conn_3G = conn_3G
         self.conn_4G = conn_4G
+        self.location_label = location_label
+        self.location_context = location_context
+        self.location_city = location_city
+
+def get_connectivity_in_city(city):
+    log.info("Getting connectivity details in %s" % city)
+    results = {}
+
+    for carrier in Carrier.query.filter(Carrier.name != None).all():
+        log.info("Analyzing %s in %s" % (carrier.name, city))
+        
+        if not results.get(carrier.name):
+            results[carrier.name] = {'2G': False, '3G': False, '4G': False}
+        
+        connectivities = CarrierConnectivity.query.filter_by(mcc_mnc = carrier.mcc_mnc, location_city = city).all()
+        for connectivity in connectivities:
+            log.info("Analyzing connectivity for %s in %s, %s" % (carrier.name, connectivity.location_label, city))
+            if connectivity.conn_2G:
+                log.info("%s has 2G connection in %s, %s" % (carrier.name, connectivity.location_label, city))
+                results[carrier.name]['2G'] = True
+            if connectivity.conn_3G:
+                log.info("%s has 3G connection in %s, %s" % (carrier.name, connectivity.location_label, city))
+                results[carrier.name]['3G'] = True
+            if connectivity.conn_4G:
+                log.info("%s has 4G connection in %s, %s" % (carrier.name, connectivity.location_label, city))
+                results[carrier.name]['4G'] = True
+
+            if results[carrier.name]['2G'] and results[carrier.name]['3G'] and results[carrier.name]['4G']:
+                log.info("%s has ALL the connectivities in %s" % (carrier.name, city))
+                break
+
+    return results
+
